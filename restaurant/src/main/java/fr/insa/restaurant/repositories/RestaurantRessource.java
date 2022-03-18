8af6d0bb70c9855fcfafcb3511848fd47225165c
Wugop <74765014+Wugop@users.noreply.github.com>
2022-03-18 23:00:23 +0100

    first init ConfigServer :wrench:

--- restaurant/src/main/java/fr/insa/restaurant/repositories/RestaurantRessource.java
+++ restaurant/src/main/java/fr/insa/restaurant/repositories/RestaurantRessource.java
@@ -3,7 +3,6 @@ package fr.insa.restaurant.repositories;
 
 import fr.insa.restaurant.model.RestaurantModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1")
 public class RestaurantRessource {
+
 
     @Autowired
     public RestaurantRepository restaurantRepository;
