bb41040a1366b120123b700875f5cf10431bbe91
tombeaussart <83578740+tombeaussart@users.noreply.github.com>
2022-03-26 09:03:34 +0100

    Gateway

---
+++ gateway/src/main/java/fr/insa/gateway/SpringCloudConfig.java
@@ -0,0 +1,20 @@
+package fr.insa.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringCloudConfig {
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/restaurant/**")
+                        .uri("lb://restaurant"))
+                .route(r -> r.path("/configserver/**")
+                        .uri("lb://configserver"))
+                .build();
+    }
+}
+
